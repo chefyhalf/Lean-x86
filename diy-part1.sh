5c8d862322f9bbc4b10efedb3779908d61cab6e9
chefyhalf <106939706+chefyhalf@users.noreply.github.com>
2022-11-16 09:32:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,8 @@
 # git clone https://github.com/immortalwrt/luci/tree/master/themes/luci-theme-openwrt-2020  package/lean/luci-theme-openwrt-2020
 # git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-packages/luci-app-bypass
 # sed -i '1i src-git bigbighill https://github.com/bigbighill/openwrt-packages-1' feeds.conf.default
-sed -i '1i src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
-sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+# sed -i '1i src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
+# sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '1i src-git liuran001 https://github.com/liuran001/openwrt-packages' feeds.conf.default
+# sed -i '1i src-git liuran001 https://github.com/liuran001/openwrt-packages' feeds.conf.default
+sed -i '1i src-git NueXini https://github.com/NueXini/NueXini_Packages' feeds.conf.default
